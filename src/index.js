f3354dc91afcb50fc287d335fe9b3719b803b2ee
dshinkarow <daniel.shinkarow@codeware.com>
2025-11-04 16:43:01 -0500

    transition between home tabs

--- src/index.js
+++ src/index.js
@@ -8,16 +8,26 @@ content.appendChild(Home());
 console.log("Webpack is working!");
 
 const homeButton = document.getElementById("home-button");
+const menuButton = document.getElementById("menu-button");
+const aboutButton = document.getElementById("about-button");
+homeButton.setAttribute("active", "true");
 homeButton.addEventListener("click", () => {
+  homeButton.setAttribute("active", "true");
+  menuButton.setAttribute("active", "false");
+  aboutButton.setAttribute("active", "false");
   content.replaceChildren(Home());
 });
 
-const menuButton = document.getElementById("menu-button");
 menuButton.addEventListener("click", () => {
+  homeButton.setAttribute("active", "false");
+  menuButton.setAttribute("active", "true");
+  aboutButton.setAttribute("active", "false");
   content.replaceChildren(Menu());
 });
 
-const aboutButton = document.getElementById("about-button");
 aboutButton.addEventListener("click", () => {
+  homeButton.setAttribute("active", "false");
+  menuButton.setAttribute("active", "false");
+  aboutButton.setAttribute("active", "true");
   content.replaceChildren(About());
 });
